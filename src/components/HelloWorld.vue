c69181d28cd84000e9c5c45d8941bf6389d7982c
misanc21 <38931684+misanc21@users.noreply.github.com>
2020-06-02 15:49:01 -0500

    recibo de json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,21 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h1>{{ titulo }}</h1>
+    <div id="div-compra">
+      <table id="tbla-compra">
+        <thead>
+          <th>SKU</th>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Price</th>
+        </thead>
+        <tbody>
+          <tr v-for>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -89,8 +25,9 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      articles: []
+      titulo: 'Articulos para comprar',
+      articles: [],
+      items: []
     }
   },
   methods: {
@@ -102,10 +39,14 @@ export default {
       }
       axios.get(`https://eshop-deve.herokuapp.com/api/v2/orders/2195721781388`,
                 config)
-      .then(({data})=>(this.articles = data))
+      .then(({data})=>{
+        this.articles = data
+        this.items = this.articles.order.items
+        })
+
     }
   },
-  created(){
+  mounted(){
     this.getArticles();
   }
 }
